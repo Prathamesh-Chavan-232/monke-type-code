44e9c4430c2ab1b638032d3955a949834b9db90a
falconcodes <prathamesh.chavanpsc2018@gmail.com>
2024-04-10 18:09:45 +0530

    feat: Simple Typing Input

---
+++ client/src/components/Textbox.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+type Props = {
+    text: string;
+};
+
+const Textbox = ({ text }: Props) => {
+    const [typedText, setTypedText] = useState("");
+    return (
+        <div className="textbox">
+            <div className="relative lookup-textbox">
+                <div className="typing-textbox">
+                    <p>{text}</p>
+                    <input
+                        className="typing-input"
+                        type="text"
+                        onChange={(e) => setTypedText(e.target.value)}
+                    />
+                    <p className="typed-text overlay">{typedText}</p>
+                    <div className="cursor blinking overlay"></div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Textbox;
